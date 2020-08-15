c437722f1df534d421d36f7f69f8a2c521d9434e
DressofBD <47344807+DressofBD@users.noreply.github.com>
2020-08-15 12:22:14 +0600

    Add files via upload

---
+++ August/Sam_Guna_F/assets/js/main.js
@@ -0,0 +1,34 @@
+  $( document ).ready(function() {
+       var shrinkHeader = 500;
+        $(window).scroll(function() {
+        var scroll = getCurrentScroll();
+          if ( scroll >= shrinkHeader ) {
+             $('.hdr-top').addClass('sticky');
+          }
+          else {
+            $('.hdr-top').removeClass('sticky');
+          }
+        });
+      function getCurrentScroll() {
+        return window.pageYOffset;
+        }
+      });
+
+
+  $("#news-ticker").eocjsNewsticker({
+    divider: "&nbsp; ",
+    interval: 30,    
+    speed: 10,
+    timeout: 0.5
+  });
+    $("#news-ticker2").eocjsNewsticker({
+    divider: "&nbsp; ",
+    interval: 30,    
+    speed: 25,
+    timeout: 0.5
+  });
+
+  // Hide menu onclick on mobile  
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
